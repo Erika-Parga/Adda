d3fcb10bde42b7a2b5761a03f88388cbf31bd69b
Erika-Parga <erika.parga.itid@unipolidgo.edu.mx>
2026-03-15 13:30:29 -0600

    CRUD eventos

--- api/routes/eventos.py
+++ api/routes/eventos.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from config.firebase import db
@@ -7,7 +9,7 @@ from datetime import datetime
 class evento(BaseModel):
     titulo: str
     ubicacion: str 
-    fecha: datetime
+    fecha: Optional[datetime] = None
     categoria: str
     img_url: str
     descripcion: str
@@ -34,6 +36,34 @@ async def obtener_eventos():
 
     return eventos
 
+@router.get("/mis-eventos")
+async def misEventos(data: tuple = Depends(verificar_centro)):
+    docEvents = db.collection("eventos")
+    uid, rol = data
+    docs_encontrados = []
+    user_ref = db.collection("usuarios").document(uid)
+    userDoc = user_ref.get()
+    evento = userDoc.to_dict()
+    centro_uid = evento["centro_uid"]
+
+    query = docEvents.where("centro_uid", "==", centro_uid)
+    resultados = query.stream()
+
+    for doc in resultados:
+        evento = doc.to_dict()
+        evento["id"] = doc.id
+        docs_encontrados.append(evento)
+
+        centro_id = evento.get("centro_uid")
+        if centro_id:
+            centroDocs = db.collection("centros").document(centro_id)
+
+            centro_doc = centroDocs.get()
+            if centro_doc.exists:
+                centro_data = centro_doc.to_dict()
+                evento["centro_nombre"] = centro_data["nombre"]
+    return docs_encontrados
+
 @router.get("/{id}")
 async def obtener_evento_por_id(id:str):
     doc_ref = db.collection("eventos").document(id)
@@ -44,9 +74,15 @@ async def obtener_evento_por_id(id:str):
             raise HTTPException(status_code=404, detail="No se encuentra ese centro")
     return evento
 
+
 @router.post("/")
 async def crear_evento(nuevoEvento: evento, data: tuple = Depends(verificar_centro)):
-    nuevo_evento = {"titulo": nuevoEvento.titulo, "ubicacion": nuevoEvento.ubicacion, "fecha":nuevoEvento.fecha,"categoria":nuevoEvento.categoria,"imagen_url":nuevoEvento.img_url, "descripcion": nuevoEvento.descripcion}
+    uid, rol = data
+    user_ref = db.collection("usuarios").document(uid)
+    userDoc = user_ref.get()
+    centro_uid = userDoc.to_dict()["centro_uid"]
+
+    nuevo_evento = {"titulo": nuevoEvento.titulo, "ubicacion": nuevoEvento.ubicacion, "fecha":nuevoEvento.fecha,"categoria":nuevoEvento.categoria,"imagen_url":nuevoEvento.img_url, "descripcion": nuevoEvento.descripcion, "centro_uid": centro_uid}
     doc_ref = db.collection("eventos").add(nuevo_evento)
     return {"mensaje": "Evento creado exitosamente", "id": doc_ref[1].id}
 
@@ -55,7 +91,9 @@ async def actualizar_evento(id:str,actualEvento: evento, data: tuple = Depends(v
     doc_ref = db.collection("eventos").document(id)
     doc = doc_ref.get()
     if doc.exists:
-        doc_ref.update({"titulo": actualEvento.titulo, "ubicacion": actualEvento.ubicacion, "fecha":actualEvento.fecha,"categoria":actualEvento.categoria,"imagen_url":actualEvento.img_url, "descripcion": actualEvento.descripcion})
+        doc_ref.update({"titulo": actualEvento.titulo, "ubicacion": actualEvento.ubicacion,"categoria":actualEvento.categoria,"imagen_url":actualEvento.img_url, "descripcion": actualEvento.descripcion})
+        if actualEvento.fecha:
+            doc_ref.update({"fecha": actualEvento.fecha})
     else:
             raise HTTPException(status_code=404, detail="No se encuentra ese evento")
     return {"mensaje": "Evento actualizado exitosamente"}   
---
+++ js/eventos.js
@@ -0,0 +1,200 @@
+protect('centro', 'index.html');
+const eventsContainer = document.getElementById('eventsContainer');
+
+async function mostrarTarjetas(eventos) {
+    eventos.forEach(evento => {
+        const div = document.createElement("div");
+        div.classList.add("evento");
+
+        div.innerHTML = `
+        <h3>${evento.titulo}</h3>
+        <p>${evento.ubicacion}</p>
+        <p>${evento.descripcion}</p>
+        <p>${evento.fecha}</p>
+        <p>${evento.categoria}</p>
+        
+        <button id="deleteButton" class="btn-eliminar" data-id="${evento.id}">Eliminar</button>
+        <button id="editButton" class="btn-editar" data-id="${evento.id}" data-id="${evento.id}"
+        data-titulo="${evento.titulo}"
+        data-categoria="${evento.categoria}"
+        data-ubicacion="${evento.ubicacion}"
+        data-descripcion="${evento.descripcion}"
+        data-fecha="${evento.fecha}">
+        Editar</button>
+        `;
+
+        eventsContainer.appendChild(div);
+    })
+}
+
+async function crearEvento(eventName, eventCat, eventDir, eventFechaHora, eventDesc) {
+    const url = `${API_URL}/eventos/`;
+    
+    try {
+        const response = await fetch(url, {
+            method: "POST",   
+            headers: {"Content-Type": "application/json",
+            "Authorization": "Bearer " + sessionStorage.getItem('authToken')
+            },
+            body: JSON.stringify({
+                titulo: eventName.value,
+                ubicacion: eventDir.value ,
+                fecha: eventFechaHora.value,
+                categoria: eventCat.value,
+                descripcion: eventDesc.value,
+                img_url:"",
+                
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        await recargarEventos();
+        
+    } catch (error) {
+        console.error(error.message)
+    }
+
+
+}
+
+async function recargarEventos() {
+    const url = `${API_URL}/eventos/mis-eventos`;
+    eventsContainer.innerHTML = ""
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {"Authorization": "Bearer " + sessionStorage.getItem('authToken')}
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const eventos = await response.json()
+        mostrarTarjetas(eventos)
+        
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function editarEvento(id, eventName, eventCat, eventDir, eventFechaHora, eventDesc) {
+    const url = `${API_URL}/eventos/${id}`;
+    
+    try {
+        const body = {
+            titulo: eventName.value,
+            ubicacion: eventDir.value,
+            categoria: eventCat.value,
+            descripcion: eventDesc.value,
+            img_url: "",
+        }
+        if (eventFechaHora.value) {
+            body.fecha = eventFechaHora.value
+        }
+        const response = await fetch(url, {
+            method: "PUT",   
+            headers: {"Content-Type": "application/json",
+            "Authorization": "Bearer " + sessionStorage.getItem('authToken')
+            },
+            body: JSON.stringify(body)
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        await recargarEventos();
+        
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+async function eliminarEvento(id) {
+    const url = `${API_URL}/eventos/${id}`;
+    
+    try {
+        const response = await fetch(url, {
+            method: "DELETE",   
+            headers: {"Content-Type": "application/json",
+            "Authorization": "Bearer " + sessionStorage.getItem('authToken')
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        await recargarEventos();
+        
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+    let modoEdicion = false        
+    let eventoEditandoId = null 
+    const url = `${API_URL}/eventos/mis-eventos`;
+    const submitBtn = document.getElementById('submitButton');
+    const eventName = document.getElementById('eventName');
+    const eventCat = document.getElementById('eventCat');
+    const eventDir = document.getElementById('eventDir');
+    const eventFechaHora = document.getElementById('eventFechaHora');
+    const eventDesc = document.getElementById('eventDesc');
+    
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {"Authorization": "Bearer " + sessionStorage.getItem('authToken')}
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const eventos = await response.json()
+        mostrarTarjetas(eventos)
+        
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+    submitBtn.addEventListener("click", async (e) => {
+        e.preventDefault()
+        if (modoEdicion){
+            await editarEvento(eventoEditandoId, eventName, eventCat, eventDir, eventFechaHora, eventDesc) 
+        }else{
+            await crearEvento(eventName, eventCat, eventDir, eventFechaHora, eventDesc)
+        }
+        modoEdicion = false      
+        eventoEditandoId = null 
+        recargarEventos()
+        eventName.value = ''
+        eventCat.value = ''
+        eventDir.value = ''
+        eventFechaHora.value = ''
+        eventDesc.value = ''
+        submitBtn.innerHTML = 'Crear evento'
+
+    })
+
+
+    eventsContainer.addEventListener('click', async(e)=>{
+        if (e.target.classList.contains('btn-eliminar')) {
+        const id = e.target.dataset.id
+        await eliminarEvento(id)
+        }
+        if (e.target.classList.contains('btn-editar')) {
+            modoEdicion = true
+            eventoEditandoId = e.target.dataset.id
+            submitBtn.innerHTML = 'Guardar cambios'
+            eventName.value = e.target.dataset.titulo;
+            eventCat.value = e.target.dataset.categoria;
+            eventDesc.value = e.target.dataset.descripcion;
+            eventDir.value = e.target.dataset.ubicacion;
+            eventFechaHora.value = e.target.dataset.fecha;
+    }
+    } )
+})
